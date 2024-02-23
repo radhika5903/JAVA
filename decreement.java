27e99b4e391a40df6427dbdaadb196716884a25c
Radhika Mittal <158038107+radhika5903@users.noreply.github.com>
2024-02-23 19:08:20 +0530

    Add files via upload

---
+++ decreement.java
@@ -0,0 +1,28 @@
+public class decreement {
+    public static void main(String[]args){
+        //int y = 7;
+        //int x = --y *7;
+        //char a = 'B';
+        //System.out.println(a++);
+       // System.out.println(a);
+       //int x =2;
+       //int y = 3;
+       //int z = x*x-y*y;
+       //System.out.println(z);
+       //int b = 1;
+       //int c = 4;
+      // int a = 5;
+       //int k = (b*b-4*a*c)/(2*a);
+       //System.out.println(k);
+       
+       int v = 2;
+       int u = 4;
+       int a = 1;
+       int S = 3;
+       int z = v*v-u*u/2*a*S;
+       System.out.println(z);
+       
+    
+
+    }
+}
