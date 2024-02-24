271b255cbcd0953274f7405364633e4d9d25a08a
Radhika Mittal <158038107+radhika5903@users.noreply.github.com>
2024-02-24 10:33:43 +0530

    Add files via upload

---
+++ cgpa.java
@@ -0,0 +1,13 @@
+public class cgpa {
+    public static void main(String[]args){
+        int sub1 = 45;
+        int sub2 = 56;
+        int sub3 = 67;
+        int sum = sub1+sub2+sub3;
+        System.out.println(sum);
+        float cgpa = sum/30.0f;
+        System.out.println(cgpa);
+
+    }
+    
+}
