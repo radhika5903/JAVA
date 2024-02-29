3a1c755f28e1a57cdefc268b8fd4d30708269f68
Radhika Mittal <158038107+radhika5903@users.noreply.github.com>
2024-02-29 23:18:02 +0530

    Add files via upload

---
+++ primitive.java
@@ -0,0 +1,15 @@
+public class primitive {
+    public static void main(String[]args){
+       // short a = 1;
+       // byte b = 127;
+       //long c = 56737467578L;
+       //char ch = 'r';
+        //double d = 4.77;
+        //double d1 = 4.77d;
+        //float f = 4.77F;
+        //boolean b1 = false;
+        String str = "Harry is a bad boy";
+        System.out.println(str);
+    }
+    
+}
