754d2c5d8b56cc2c16b4e429f0dfbd1c91e8c8fb
Radhika Mittal <158038107+radhika5903@users.noreply.github.com>
2024-02-29 23:31:03 +0530

    Add files via upload

---
+++ operators.java
@@ -0,0 +1,32 @@
+public class operators {
+    public static void main(String[]args){
+        //assignment operators
+        //int a = 5;
+        //a += 5;
+        //output = 10
+        //int b = 5;
+        //b *= 4;
+        //output = 20
+        
+        //comparision operators
+        System.out.println(65>55); //true
+
+        //logical operators
+        //System.out.println(65>=65 && 87>87); //false
+        //System.out.println(65==68 || 87>=87); //true
+        //System.out.println(67!=67);//false
+
+        //module operators
+        // float r = 8.1f;
+        //float h= r%4.2f;
+        //output = 3.900006
+        int a = 7;
+        int b = a%2;
+        System.out.println(b);
+        //output = 1
+
+
+
+
+    }
+}
