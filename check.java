5d4c87d3f2f81989dacefbf716deeead08c79886
Radhika Mittal <158038107+radhika5903@users.noreply.github.com>
2024-03-01 08:29:15 +0530

    Add files via upload

---
+++ check.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class check {
+    public static void main(String[]args){
+        System.out.println("Taking input from the user ");
+        Scanner sc = new Scanner(System.in);
+        boolean b1 = sc.hasNext();
+        System.out.println(b1);
+    }       
+}
